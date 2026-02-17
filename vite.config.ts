a31bd19b03b76fe8bc53e5c1ce65dac55f805df5
Carlos Pacheco <mauripacheco2004@gmail.com>
2026-02-16 23:24:02 -0500

    Re-ajuste importación de imagenes

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
             input: [
                 ...glob.sync('resources/css/*.css'),
                 ...glob.sync('resources/js/**/*.js'),
-                ...glob.sync('resources/images/**')
+                ...glob.sync('resources/images/**/*.jpg')
             ],
             ssr: 'resources/js/ssr.ts',
             refresh: true,
